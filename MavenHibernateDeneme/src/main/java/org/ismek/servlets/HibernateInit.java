43e3b1058d1a96a4a0cc18b52060d3029978d80a
Lab05-01 <Lab05-01@Lab08-13>
2019-03-30 15:35:32 +0200

    MavenHibernateDeneme

---
+++ MavenHibernateDeneme/src/main/java/org/ismek/servlets/HibernateInit.java
@@ -0,0 +1,30 @@
+package org.ismek.servlets;
+
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateInit {
+	private static final SessionFactory sessionFactory = buildSessionFactory();
+	
+	private static SessionFactory buildSessionFactory() {
+		try {
+			Configuration configration = new Configuration().configure();
+			StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+					.applySettings(configration.getProperties());
+			return configration.buildSessionFactory(builder.build());
+		} catch (Exception e) {
+			System.err.println("SessionFactory olusturlamadi");
+			throw new ExceptionInInitializerError(e);
+		}
+	}
+	
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	
+	public static void shutdown() {
+		getSessionFactory().close();
+	}
+}
